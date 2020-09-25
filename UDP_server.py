12310b12cfb4d63b1a9113b0cb47553c271ea5f8
sixbrigands <dcalhq@gmail.com>
2020-09-24 20:19:25 -0400

    fix responses

--- UDP_server.py
+++ UDP_server.py
@@ -14,15 +14,15 @@ while True:
         break
     message = data.decode('utf-8')
     print(message)
-    mess_arr = message.split() #convert to list with [header, host, port, connect_ID]
-    connect_ID = int(mess_arr[3])
+    mess_arr = message.split() #convert to list with [header, connect_ID, host, port]
+    connect_ID = int(mess_arr[1])
     #if the connection ID is not in the ID_arr, then return an OK message
-    if connect_ID  not in ID_arr: 
-        ID_arr.append(connect_ID )
-        response = "OK " + mess_arr[3] + " " + mess_arr[1] + " " + mess_arr[2]
+    if connect_ID not in ID_arr: 
+        ID_arr.append(connect_ID)
+        response = "OK " + mess_arr[1] + " " + mess_arr[2] + " " + mess_arr[3]
         s.sendto(bytes(response, 'utf-8'), (addr))
 
     #else tell the client to reset the connection ID
     else:
-        response = "RESET " + mess_arr[3]
+        response = "RESET " + mess_arr[1]
         s.sendto(bytes(response, 'utf-8'), (addr))
