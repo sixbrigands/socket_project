c48594fe75f2f51c4cd9ee49bbcb1293e8341d08
sixbrigands <dcalhq@gmail.com>
2020-09-24 20:10:51 -0400

    return RESET response

--- UDP_server.py
+++ UDP_server.py
@@ -2,7 +2,8 @@ import socket
 import sys
 
 host = sys.argv[1]  #server port and address      
-port = int(sys.argv[2])  
+port = int(sys.argv[2])
+ID_arr = []  
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 s.bind((host, port))
@@ -13,6 +14,15 @@ while True:
         break
     message = data.decode('utf-8')
     print(message)
-    message = message.split() #convert to list with [header, host, port, connect_ID]
-    response = "OK" + " " + message[3] + " " + message[1] + " " + message[2]
-    response = s.sendto(bytes(response, 'utf-8'), (addr))
+    mess_arr = message.split() #convert to list with [header, host, port, connect_ID]
+    connect_ID = int(mess_arr[3])
+    #if the connection ID is not in the ID_arr, then return an OK message
+    if connect_ID  not in ID_arr: 
+        ID_arr.append(connect_ID )
+        response = "OK " + mess_arr[3] + " " + mess_arr[1] + " " + mess_arr[2]
+        s.sendto(bytes(response, 'utf-8'), (addr))
+
+    #else tell the client to reset the connection ID
+    else:
+        response = "RESET " + mess_arr[3]
+        s.sendto(bytes(response, 'utf-8'), (addr))
